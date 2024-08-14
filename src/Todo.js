96f2a1227fc5ff8d7d00e84fd1a5e7b2d46927d5
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-08-14 13:37:31 +0530

    added local storage and change design

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const generateId = () => {
   return Math.floor(Math.random() * 1000);
@@ -8,54 +10,73 @@ function Todo() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
 
+  // Load todos from local storage when component mounts
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (storedTodos.length !== 0) setTodos(storedTodos);
+  }, []);
+
+  // save todos to localstorage whenever "todos" changes
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   const handleSubmit = () => {
-    setTodos((todos) =>
+    setTodos(
       todos.concat({
         text: input,
         id: generateId(),
       })
     );
-
+    toast.success("Task added successfully !");
     setInput("");
   };
 
   const removeTodo = (id) => {
-    setTodos((todos) => todos.filter((t) => t.id !== id));
+    setTodos(todos.filter((t) => t.id !== id));
+    toast.error("Task deleted successfully !");
   };
+
   return (
     <>
       <div
-        className="d-flex flex-column justify-content-center align-content-center vh-100"
-        style={{ backgroundColor: "blueviolet" }}
+        id="todo"
+        className="d-flex justify-content-center align-items-center vh-100"
       >
         <div
-          className="p-5"
+          className="p-3 shadow border"
           style={{
-            backgroundColor: "white",
-            margin: "auto",
+            width: "325px",
+            borderRadius: "25px",
           }}
         >
-          <input
-            type="text"
-            placeholder="Enter your task"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-
-          <button className="submit-button" onClick={handleSubmit}>
-            Submit
-          </button>
+          <h3 className="text-white mb-3">Todo App</h3>
+          <div className="d-flex justify-content-around mb-4">
+            <input
+              type="text"
+              placeholder="Enter the task"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button className="submit-button" onClick={handleSubmit}>
+              +
+            </button>
+          </div>
 
           <ul>
-            {todos.map(({ text, id }) => (
-              <li key={id}>
-                <span>{text}</span>
-                <button className="close-button" onClick={() => removeTodo(id)}>
+            {todos.map((todo) => (
+              <li key={todo.id}>
+                <span>{todo.text}</span>
+                <button
+                  className="close-button"
+                  onClick={() => removeTodo(todo.id)}
+                >
                   X
                 </button>
               </li>
             ))}
           </ul>
+          <ToastContainer />
         </div>
       </div>
     </>
